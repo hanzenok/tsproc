091512a5877fa00b9f5340e3ba75135c8271ea28
Mykhailo Ganza <hanzenok@gmail.com>
2016-07-04 11:58:37 +0200

    Modified the documentation

---
+++ out/api.js
@@ -0,0 +1,19 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "tsinterp",
+        "tsproc",
+        "tsreductions"
+    ],
+    "modules": [
+        "tsproc"
+    ],
+    "allModules": [
+        {
+            "displayName": "tsproc",
+            "name": "tsproc"
+        }
+    ],
+    "elements": []
+} };
+});
